189882943298aaa6c9b8a8afe8f317660aaa430d
Deniz Duijker <duijker.d@hsleiden.nl>
2026-01-07 09:32:38 +0100

    Drop shell script in favour of directly using python in command section

--- lca.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-SCRIPTDIR=$(dirname "$(readlink -f "$0")")
-python $SCRIPTDIR"/lca.py" -i $1 -o $2 -b $3 -id $4 -cov $5 -t $6 -tid $7 -tcov $8 -fh $9 -flh "${10}" -minbit "${11}" -fs "${12}"
